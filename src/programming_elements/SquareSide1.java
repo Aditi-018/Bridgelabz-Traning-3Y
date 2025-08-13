52fb5bc8c34f6ac5583c6eee5edd7ac61b592ef7
Aditi Sahu <aditisahu.in@gmail.com>
2025-08-13 14:45:05 +0530

    Added level 1 and level 2 programs

---
+++ src/programming_elements/SquareSide1.java
@@ -0,0 +1,15 @@
+package programming_elements;
+
+import java.util.Scanner;
+
+public class SquareSide1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter perimeter of square: ");
+        double perimeter = sc.nextDouble();
+
+        double side = perimeter / 4;
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
